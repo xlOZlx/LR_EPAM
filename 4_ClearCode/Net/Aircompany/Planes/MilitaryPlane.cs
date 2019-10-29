51f782b4a22abe0d8c11c9f6ba356e4c1122676f
xlOZlx <dima1562.17@gmail.com>
2019-10-29 15:17:23 +0300

    update name foulder

---
+++ 4_ClearCode/Net/Aircompany/Planes/MilitaryPlane.cs
@@ -0,0 +1,38 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        private MilitaryType militaryType;
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType militaryType)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            this.militaryType = militaryType;
+        }
+
+        public override bool Equals(object objectToCompare)
+        {
+            return (objectToCompare as MilitaryPlane) != null &&
+                   base.Equals(objectToCompare) &&
+                   militaryType == (objectToCompare as MilitaryPlane).militaryType;
+        }
+
+        public override int GetHashCode()
+        {
+            int coefficient = -1521134295;
+            return ((1701194404 * coefficient) + base.GetHashCode()) * -1521134295 + militaryType.GetHashCode();
+        }
+
+        public MilitaryType GetPlaneType()
+        {
+            return militaryType;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}", ", type=" + militaryType + '}');
+        }        
+    }
+}
---
+++ 4_ClearCode/Net/Aircompany/Planes/Plane.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Aircompany.Planes
+{
+    public abstract class Plane
+    {
+        private string planeModel;
+        private int planeMaxSpeed;
+        private int planeMaxFlightDistance;
+        private int planeMaxLoadCapacity;
+
+        public Plane(string planeModel, int planeMaxSpeed, int planeMaxFlightDistance, int planeMaxLoadCapacity)
+        {
+            this.planeModel = planeModel;
+            this.planeMaxSpeed = planeMaxSpeed;
+            this.planeMaxFlightDistance = planeMaxFlightDistance;
+            this.planeMaxLoadCapacity = planeMaxLoadCapacity;
+        }
+
+        public string GetPlaneModel()
+        {
+            return this.planeModel;
+        }
+
+        public int GetPlaneMaxSpeed()
+        {
+            return this.planeMaxSpeed;
+        }
+
+        public int GetPlaneMaxFlightDistance()
+        {
+            return this.planeMaxFlightDistance;
+        }
+
+        public int GetPlaneMaxLoadCapacity()
+        {
+            return this.planeMaxLoadCapacity;
+        }
+
+        public override string ToString()
+        {
+            return "Plane{ model='" + this.planeModel + '\'' +
+                ", maxSpeed=" + this.planeMaxSpeed +
+                ", maxFlightDistance=" + this.planeMaxFlightDistance +
+                ", maxLoadCapacity=" + this.planeMaxLoadCapacity + '}';
+        }
+
+        public override bool Equals(object objectToCompare)
+        {
+            return (objectToCompare as Plane) != null &&
+                   planeModel == (objectToCompare as Plane).planeModel &&
+                   planeMaxSpeed == (objectToCompare as Plane).planeMaxSpeed &&
+                   planeMaxFlightDistance == (objectToCompare as Plane).planeMaxFlightDistance &&
+                   planeMaxLoadCapacity == (objectToCompare as Plane).planeMaxLoadCapacity;
+        }
+
+        public override int GetHashCode()
+        {
+            int coefficient = -1521134295;
+            return ((((-1043886837 * coefficient) + EqualityComparer<string>.Default.GetHashCode(planeModel))
+                * coefficient + planeMaxSpeed.GetHashCode()) 
+                * coefficient + planeMaxFlightDistance.GetHashCode())
+                * coefficient + planeMaxLoadCapacity.GetHashCode();
+        }        
+
+    }
+}
